07be47332e24e9341bf455ad1f8d9db056fd9cad
ashxjain <ashxjain@gmail.com>
2014-02-09 11:48:07 +0530

    First Commit

---
+++ ICMPPacket.java
@@ -0,0 +1,60 @@
+import java.applet.Applet;
+import java.awt.Label;
+import java.awt.Button;
+import java.awt.Scrollbar;
+import java.awt.TextField;
+import java.awt.TextArea;
+import java.awt.Event;
+import java.awt.Choice;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Image;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Panel;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+import javax.swing.table.DefaultTableModel;
+class ICMPPacket extends Panel
+{
+	Label head;
+	JTable table;
+	int routers;
+	DefaultTableModel tableModel = new DefaultTableModel(
+	new Object[][] 
+	{
+		{"Source IP", "192.168.11.01"},
+        {"Destination IP", "125.21.167.29  "},
+        {"TTL","1"},
+		{"ICMP Type","8"},//"EchoReqT"},
+		{"ICMP Code","0"},//"EchoReqC"},
+	},
+    new Object[] 
+	{"Description", "value" })
+	{ 
+		@Override  
+    	public boolean isCellEditable(int row, int column)  
+    	{  
+			return false;  
+    	} 
+	};	
+	public ICMPPacket(int routers,Color c)
+	{
+		setBackground(c);
+		this.routers = routers;
+		head =new Label("ICMP PACKET");
+		head.setBackground(Color.blue);
+		head.setForeground(Color.white);
+		add(head);
+		TableColumn column = null;
+		table = new JTable(tableModel);
+		column = table.getColumnModel().getColumn(0);
+		column.setPreferredWidth(110);
+		column = table.getColumnModel().getColumn(1);
+        column.setPreferredWidth(120);
+		add(table);
+	}
+}
