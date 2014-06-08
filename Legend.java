566e6075d71e951915842a18e8abe5b5edfe91fe
ashxjain <ashxjain@gmail.com>
2014-06-08 22:12:27 +0530

    Updating Legend

--- Legend.java
+++ Legend.java
@@ -32,40 +32,34 @@ class Legend extends Canvas
 	{
 			d = size();
 			int w = (int)d.getWidth()-10;
-			int h = (int)d.getHeight()/2;
+			int h = (int)d.getHeight()-10;
+            int startx = 10;
+            int starty = 30;
 			g.draw3DRect(0,0,w,h,true);
+            g.drawString("LEGEND:",startx,starty);
 			if(TraceRoute.client.getSelectedIndex()==0)
 			{
 				g.setColor(Color.blue);
-				g.fillRect(10,10,packetWidth,packetHeight);
-				g.drawString("UDP packet",20+packetWidth,20);
+				g.fillRect(startx+10,starty+20,packetWidth,packetHeight);
+				g.drawString("UDP packet",startx+20+packetWidth,starty+30);
 				g.setColor(Color.red);
-		        g.fillRect(10,20+packetHeight,packetWidth,packetHeight);	
-				g.drawString("ICMP Time Exceeded Error Message",20+packetWidth,30+packetHeight);
+		        g.fillRect(startx+10,starty+30+packetHeight,packetWidth,packetHeight);	
+				g.drawString("ICMP Time Exceeded Error Message",startx+20+packetWidth,starty+40+packetHeight);
 				g.setColor(Color.black);
-		        g.fillRect(10,30+packetHeight+packetHeight,packetWidth,packetHeight);
-				g.drawString("ICMP Destination Unreachable Error Message",20+packetWidth,40+packetHeight+packetHeight);
+		        g.fillRect(startx+10,starty+40+packetHeight+packetHeight,packetWidth,packetHeight);
+				g.drawString("ICMP Destination Unreachable Error Message",startx+20+packetWidth,starty+50+packetHeight+packetHeight);
 			}
 			else
 			{
 				g.setColor(Color.blue);
-				g.fillRect(10,10,packetWidth,packetHeight);
-				g.drawString("ICMP Echo Request",20+packetWidth,20);
+				g.fillRect(startx+10,starty+20,packetWidth,packetHeight);
+				g.drawString("ICMP Echo Request",startx+20+packetWidth,starty+30);
 				g.setColor(Color.red);
-		        g.fillRect(10,20+packetHeight,packetWidth,packetHeight);	
-				g.drawString("ICMP Time Exceeded Error Message",20+packetWidth,30+packetHeight);
+		        g.fillRect(startx+10,starty+30+packetHeight,packetWidth,packetHeight);	
+				g.drawString("ICMP Time Exceeded Error Message",startx+20+packetWidth,starty+40+packetHeight);
 				g.setColor(Color.black);
-		        g.fillRect(10,30+packetHeight+packetHeight,packetWidth,packetHeight);
-				g.drawString("ICMP Reply Message",20+packetWidth,40+packetHeight+packetHeight);
+		        g.fillRect(startx+10,starty+40+packetHeight+packetHeight,packetWidth,packetHeight);
+				g.drawString("ICMP Reply Message",startx+20+packetWidth,starty+50+packetHeight+packetHeight);
 			}
-			g.setColor(Color.black);
-			//g.draw3DRect(0,h+10,w,h-32,true);
-			g.draw3DRect(0,h,w,h,true);
-			String s = "Traceroute Applet written by Ashish Jain & Afaaq Alam";
-			g.drawString(s,10,h+40);
-			s = "Advised by Dr.Ram Prakash Rustagi, rprustagi@pes.edu";
-            g.drawString(s,10,h+60);
-            s = "PESIT, Bangalore, India";
-            g.drawString(s,10,h+80);
     }
 }
